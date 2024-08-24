fcfa4f211a88bae7cb221764fbedf8350b4ca365
Tereza-Khachatryan <saterya97@gmail.com>
2024-08-24 03:04:09 -0700

    binary operator

---
+++ GTU.binary+operator.js
@@ -0,0 +1,30 @@
+null + undefined  //Error  NaN
+null + true  //1
+null + false  // 0
+null + 3  // 3
+null + NaN  //NaN
+null + 'hello'  // 'nullhello'
+null + a + b + c + d + e + f  //Error
+undefined + true  //NaN
+undefined + false  //NaN
+undefined + 5  // NaN
+undefined + NaN  //NaN
+undefined + 'hello' // undefinedhello
+undefined + a + b + c + d + e + f  // Error
+true + false // 1
+true + 2 //3
+true + NaN // NaN
+true + 'hello' // truehello
+true + a + b + c + d + e + f  //error
+false + 4 // 4
+false + NaN //NaN
+false + 'hello' // falsehello
+false + a + b + c + d + e + f //error
+5 + 3 // 8
+5 + NaN //NaN
+5 + 'hello' // 5hello
+5 + a + b + c + d + e + f //error
+'hello' + NaN // helloNaN
+'hello' + a + b + c + d + e + f //error
+
+
